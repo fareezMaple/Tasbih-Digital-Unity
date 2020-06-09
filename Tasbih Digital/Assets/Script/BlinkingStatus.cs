8cf8983fe00f52e43303a2213678729120dcca8a
iqFareez <60868965+fareezMaple@users.noreply.github.com>
2020-06-10 07:45:08 +0800

    Finish writing blink script https://answers.unity.com/questions/1112861/how-can-i-make-a-ui-image-blink-on-and-off.html

--- Tasbih Digital/Assets/Script/BlinkingStatus.cs
+++ Tasbih Digital/Assets/Script/BlinkingStatus.cs
@@ -1,18 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BlinkingStatus : MonoBehaviour
 {
+    public GameObject blinkingBar;
+    private Image barColourImage;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        barColourImage = gameObject.GetComponent<Image>();
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Blink()
     {
-        
+        while (true)
+        {
+            switch (barColourImage.color.a.ToString()) 
+            {
+                case "0":
+                    var color = barColourImage.color;
+                    color = new Color(color.r, color.g, color.b, 1);
+                    barColourImage.color = color;
+                    yield return new WaitForSeconds(.5f);
+                    break;
+                case "1":
+                    var color1 = barColourImage.color;
+                    color1 = new Color(color1.r, color1.g, color1.b, 0);
+                    barColourImage.color = color1;
+                    yield return new WaitForSeconds(.5f);
+                    break;    
+            }
+        }
     }
-}
+
+    private void StartBlinking()
+    {
+        StopAllCoroutines();
+        StartCoroutine(Blink());
+    }
+
+    private void StopBlinking()
+    {
+        StopAllCoroutines();
+    }
+    
+} //https://answers.unity.com/questions/1112861/how-can-i-make-a-ui-image-blink-on-and-off.html source
