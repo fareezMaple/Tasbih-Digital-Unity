99da49e7116b1faa82d349853f3d5c2e75076ea4
iqFareez <60868965+fareezMaple@users.noreply.github.com>
2020-06-20 06:16:18 +0800

    Update ModeChanger.cs
    fix infinte loop

--- Tasbih Digital/Assets/Script/ModeChanger.cs
+++ Tasbih Digital/Assets/Script/ModeChanger.cs
@@ -3,24 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class ModeChanger : MonoBehaviour
 {
-    private string LAST_MODE = "lastMode";
-    private void Awake()
-    {
-        SceneManager.LoadScene(PlayerPrefs.GetInt(LAST_MODE, 0));
-    }
 
     public void changeMode()
     {
         if (SceneManager.GetActiveScene().buildIndex == 0)
         {
             SceneManager.LoadScene(1); //load dark scene
-            
-        } else if (SceneManager.GetActiveScene().buildIndex == 1)
+
+        }
+        else if (SceneManager.GetActiveScene().buildIndex == 1)
         {
             SceneManager.LoadScene(0); //load light scene
         }
-        
-        PlayerPrefs.SetInt(LAST_MODE, SceneManager.GetActiveScene().buildIndex);
     }
-    
 }
